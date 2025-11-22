b9131b88384af312f728357f7d911185b58e78a8
a4grafica <barbieripabloa@gmail.com>
2025-11-22 11:25:33 -0300

    Fix final: Agregada extensión .js a multerConfig para Render

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 <<<<<<< HEAD
-const upload = require("../middlewares/multerConfig");
+const upload = const upload = require('../middlewares/multerConfig.js'); // ¡Con la extensión .js!
 const uploadCtrl = require("../controllers/uploadController");
 const jobsCtrl = require("../controllers/jobsController");
 const mpWebhookCtrl = require("../controllers/mpWebhookController");
